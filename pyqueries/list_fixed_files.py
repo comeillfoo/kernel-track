c09f590cfbcb416cd19a5881f3199f799eb85e43
come_ill_foo <rayveleaveme34@inbox.ru>
2023-12-05 00:34:02 +0000

    feat(pyqueries): list files fixed by cve

---
+++ pyqueries/list_fixed_files.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import argparse
+from models.LxKernelCve import LxKernelCve
+import logging
+
+
+ENOENT = 2
+
+def parser() -> argparse.ArgumentParser:
+    p = argparse.ArgumentParser('list_fixed_files')
+    p.add_argument('cve', help='CVE id')
+
+    p.add_argument('-q', '--brief', action='store_true')
+    return p
+
+
+def main() -> int:
+    args = parser().parse_args()
+    cveid, brief = args.cve, args.brief
+
+    logging.getLogger().setLevel('WARNING' if brief else 'INFO')
+
+    logging.info(f'List fixed files for {cveid}')
+
+    LxKernelCve.loadDb()
+    logging.info('Linux Kernel Cves data loaded')
+
+    lxKernelCve = LxKernelCve.select(cveid)
+    if not lxKernelCve:
+        logging.error(f'cannot found vulnerability by CVE: {cveid}')
+        return ENOENT
+
+    if lxKernelCve.fixes == '':
+        logging.warning('no files available')
+        return ENOENT
+
+    fixedFiles = lxKernelCve.fixed_files()
+    if len(fixedFiles) == 0:
+        logging.warning('no files were fixed')
+        return ENOENT
+
+    for fixedFile in fixedFiles:
+        print(fixedFile)
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
+
--- pyqueries/models/LxKernelCve.py
+++ pyqueries/models/LxKernelCve.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 from typing import Tuple
+import requests, json
+from bs4 import BeautifulSoup
+from pprint import pprint
 
 
 class LxKernelCve:
@@ -19,6 +22,11 @@ class LxKernelCve:
         self.last_modified = cvedata.get('last_modified', '')
         self.nvd_text = cvedata.get('nvd_text', '')
 
+
+    def cve(self) -> str:
+        return 'CVE-' + self.id
+
+
     def ref_url(self, source: str) -> str:
         prefixes = {
             'Debian': 'https://security-tracker.debian.org/tracker/CVE-',
@@ -35,11 +43,34 @@ class LxKernelCve:
         return prefix + self.id
 
 
+    def fixed_files(self) -> list[str]:
+        if self.fixes == '':
+            return []
+
+        url = 'https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit?id='
+
+        soup = BeautifulSoup(requests.get(url + self.fixes).text, 'html.parser') # TODO errors handling
+
+        diffstat = soup.find('table', { 'class': 'diffstat' })
+
+        def _get_file(row) -> str:
+            return row.find('td', { 'class': 'upd' }).string.strip()
+
+        return [ _get_file(row) for row in diffstat.find_all('tr', recursive=False) ]
+
+
+    cve_source = 'https://raw.githubusercontent.com/nluedtke/linux_kernel_cves/master/data/kernel_cves.json'
     db = {}
     @classmethod
-    def __getitem__(cls, cveid: str):
+    def select(cls, cveid: str):
         return cls.db.get(cveid, None)
 
     @classmethod
-    def __setitem__(cls, cveid: str, cvedata: dict):
+    def insert(cls, cveid: str, cvedata: dict):
         cls.db[cveid] = LxKernelCve(cveid, cvedata)
+
+    @classmethod
+    def loadDb(cls):
+        kernel_cves = json.loads(requests.get(cls.cve_source).text) # TODO errors handling
+        for cveid, cvedata in kernel_cves.items():
+            LxKernelCve.insert(cveid, cvedata)
